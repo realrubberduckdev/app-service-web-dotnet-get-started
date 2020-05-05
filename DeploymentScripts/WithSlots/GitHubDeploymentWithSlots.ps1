5b9edb66a1adbfd7a7cce19488c4bb67716cdbc7
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2020-05-05 16:43:31 +0100

    Update script to work with latest api. Create resource if needed.

--- DeploymentScripts/WithSlots/GitHubDeploymentWithSlots.ps1
+++ DeploymentScripts/WithSlots/GitHubDeploymentWithSlots.ps1
@@ -1,19 +1,24 @@
 [CmdletBinding()]
 Param(
     [Parameter()]
-    [string]$resourceGroupName = "web-deployment-test-rg"
+    [string]$resourceGroupName = "web-deployment-test-rg",
+    [Parameter()]
+    [string]$webappname = "webapp-github-deployment-with-slots-" + $(Get-Random)
 )
 
 # Replace the following URL with a public GitHub repo URL
 $gitrepo="https://github.com/realrubberduckdev/app-service-web-dotnet-get-started.git"
-$webappname="mywebapp$(Get-Random)"
 $location="UK South"
 
 # Connect to Azure subscription
 Connect-AzAccount
 
-# Create a resource group.
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+Get-AzResourceGroup -Name $resourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent)
+{
+    # Create a resource group.
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+}
 
 # Create an App Service plan in Free tier.
 New-AzAppServicePlan -Name $webappname -Location $location `
@@ -38,8 +43,8 @@ $PropertiesObject = @{
 }
 Set-AzResource -PropertyObject $PropertiesObject -ResourceGroupName $resourceGroupName `
 -ResourceType Microsoft.Web/sites/slots/sourcecontrols `
--ResourceName $webappname/staging/web -ApiVersion 2015-08-01 -Force
+-ResourceName $webappname/staging/web -ApiVersion 2019-08-01 -Force
 
 # Swap the verified/warmed up staging slot into production.
 Switch-AzWebAppSlot -Name $webappname -ResourceGroupName $resourceGroupName `
--SourceSlotName staging -DestinationSlotName productiona
+-SourceSlotName staging -DestinationSlotName production
